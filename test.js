9923a75021e2128bac370f674f37731224c228ec
andre-matulionis-ifood <andre.matulionis@ifood.com.br>
2019-08-31 11:58:16 -0300

    Fix children Finally parsing correctly

--- test.js
+++ test.js
@@ -1,12 +1,25 @@
-const fs = require("fs");
-const { artParser } = require("./index.js");
+const fs = require('fs')
+const args = process.argv.slice(2)
+const { artParser } = require('./index.js')
 
-const artFile = fs.readFileSync("./example0.art", { encoding: "UTF-8" });
+const fileName = args[0]
 
-const parseResponse = artParser.run(artFile);
+if (!fileName) {
+  console.error('A file name should be specified')
+  process.exit(1)
+}
+
+const artFile = fs.readFileSync(fileName, { encoding: 'UTF-8' })
+
+const artTarget = artFile
+  .split('\n')
+  .map(line => line.trim())
+  .join('')
+
+const parseResponse = artParser.run(artTarget)
 
 if (parseResponse.isError) {
-  console.error(parseResponse.error);
+  console.error(parseResponse.error)
 } else {
-  // console.log(JSON.stringify(parseResponse.result, null, 2));
+  console.log(JSON.stringify(parseResponse.result, null, 2))
 }
